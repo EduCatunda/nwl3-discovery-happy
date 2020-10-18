9602cba0aa1fe29f8952b3c883ae490216120f31
Eduardo Catunda <EduCatunda@users.noreply.github.com>
2020-10-18 19:45:51 -0300

    Upload do projeto.

---
+++ src/database/fakedata.js
@@ -0,0 +1,35 @@
+module.exports = [
+    {
+        id: 1,
+        name: "Lar das meninas",
+        lat: "-3.7279047",
+        long: "-38.5341456",
+        description: "Presta assitência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        image: [
+            "https://images.unsplash.com/photo-1567791124560-c60b7d227623?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9", 
+            
+            "https://images.unsplash.com/photo-1597730945481-f35a5cf39021?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Presta",
+        opening_hours: "Horario",
+        opening_weekends: "1"
+
+    },
+
+
+
+    {
+        id: 2,
+        lat: "-3.7465672",
+        long: "-38.53117345",
+        description: "Presta assitência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        image: [
+            "https://images.unsplash.com/photo-1567791124560-c60b7d227623?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9", 
+            
+            "https://images.unsplash.com/photo-1597730945481-f35a5cf39021?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Presta",
+        opening_hours: "Horario",
+        opening_weekends: "0"
+    }
+]
